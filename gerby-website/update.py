4b7ba3c6addaf536ec291fdef856f5227334c9e0
Christopher Zhou <czhou3@caltech.edu>
2020-12-01 17:47:48 -0600

    update bootstrapper

--- gerby-website/update.py
+++ gerby-website/update.py
@@ -12,14 +12,14 @@ logging.basicConfig(stream=sys.stdout)
 log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
 
-diff = subprocess.Popen(['git', 'diff', 'master:document.tex', '--', '../document.tex'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+diff = subprocess.Popen(['git', 'diff', 'master:document.tex', '--', 'document.tex'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd='/root/mipstar/')
 out, err = diff.communicate()
 
 log.info(out)
 log.error(err)
 
 if not err:
-    pull = subprocess.Popen(['git', 'pull', 'origin', 'master'], cwd='/root/mipstar/', stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    pull = subprocess.Popen(['git', 'pull', 'origin', 'master'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd='/root/mipstar/')
     o, e = pull.communicate()
     log.info(o)
     log.error(e)
@@ -35,12 +35,12 @@ if not err:
         log.info(o)
         log.error(e)
 
-        update = subprocess.Popen(['python3', 'update.py'], cwd='/root/mipstar/gerby-website/gerby/', stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        update = subprocess.Popen(['python3', 'update.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd='/root/mipstar/gerby-website/gerby/')
         o, e = update.communicate()
         log.info(o)
         log.error(e)
 
-    restart = subprocess.Popen(['sudo', 'systemctl', 'restart', 'mipstar'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    restart = subprocess.Popen(['sudo', 'systemctl', 'restart', 'mipstar'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd='/root/mipstar/')
     o, e = restart.communicate(input=bytes(AUTH, 'utf-8'))
 
     log.info(o)
