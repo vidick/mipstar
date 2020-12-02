590ed231bd3d61e41514ea7d176ac55bcca4c1bd
Christopher Zhou <czhou3@caltech.edu>
2020-12-02 11:41:38 -0600

    update bootstrapper

--- update.py
+++ update.py
@@ -19,6 +19,7 @@ log.setLevel(logging.INFO)
 
 # diff = subprocess.Popen(['git', 'diff', 'master:document.tex', '--', 'document.tex'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd='/root/mipstar/')
 # out, err = diff.communicate()
+log.info('  Checking diff between local and remote document.tex')
 stdin, stdout, stderr = ssh.exec_command('cd /root/mipstar && git diff master:document.tex -- document.tex')
 out = stdout.readlines()
 err = stderr.readlines()
@@ -30,17 +31,19 @@ if err:
 if not err:
     # pull = subprocess.Popen(['git', 'pull', 'origin', 'master'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd='/root/mipstar/')
     # o, e = pull.communicate()
+    log.info('  Pulling from remote master branch')
     stdin, stdout, stderr = ssh.exec_command('cd /root/mipstar && git pull origin master')
     o = stdout.readlines()
     e = stderr.readlines()
     if o:
-        log.info(''.join(o))
+        log.info('\n' + ''.join(o))
     if e:
-        log.error(''.join(e))
+        log.error('\n' + ''.join(e))
 
     if out:
         # gen_tags = subprocess.Popen(['python3', 'tagger.py', 'document.tex', '>', 'tags'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd='/root/mipstar/')
         # o, e = gen_tags.communicate()
+        log.info('  Updating document tags')
         stdin, stdout, stderr = ssh.exec_command('cd /root/mipstar && python3 tagger.py document.tex > tags')
         o = stdout.readlines()
         e = stderr.readlines()
@@ -51,6 +54,7 @@ if not err:
 
         # gen_doc = subprocess.Popen(['plastex', '--renderer=Gerby', './document.tex'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd='/root/mipstar/')
         # o, e = gen_doc.communicate()
+        log.info('  Rendering document.tex using plastex')
         stdin, stdout, stderr = ssh.exec_command('cd /root/mipstar && plastex --renderer=Gerby ./document.tex')
         o = stdout.readlines()
         e = stderr.readlines()
@@ -61,6 +65,7 @@ if not err:
 
         # update = subprocess.Popen(['python3', 'update.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd='/root/mipstar/gerby-website/gerby/')
         # o, e = update.communicate()
+        log.info('  Updating database with new tags')
         stdin, stdout, stderr = ssh.exec_command('cd /root/mipstar/gerby-website/gerby/tools && python3 update.py')
         o = stdout.readlines()
         e = stderr.readlines()
@@ -68,9 +73,11 @@ if not err:
             log.info(''.join(o))
         if e:
             log.error(''.join(e))
-
+    else:
+        log.info('  No diff found')
     # restart = subprocess.Popen(['sudo', 'systemctl', 'restart', 'mipstar'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd='/root/mipstar/')
     # o, e = restart.communicate(input=bytes(AUTH, 'utf-8'))
+    log.info('  Requesting service restart')
     stdin, stdout, stderr = ssh.exec_command('sudo systemctl restart mipstar')
     stdin.write(AUTH + '\n')
     o = stdout.readlines()
