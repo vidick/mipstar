89dafb43a0608229d3f4f9c3dfb5b5e4acfa22e2
Christopher Zhou <czhou3@caltech.edu>
2020-12-11 15:05:27 -0600

    update update.py

--- update.py
+++ update.py
@@ -20,7 +20,7 @@ log.setLevel(logging.INFO)
 
 # diff = subprocess.Popen(['git', 'diff', 'master:document.tex', '--', 'document.tex'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd='/root/mipstar/')
 # out, err = diff.communicate()
-log.info('  Checking diff between local and remote document.tex')
+log.info('  Checking diff between local and remote tex files')
 stdin, stdout, stderr = ssh.exec_command('cd /root/mipstar && git fetch --prune')
 o = stdout.readlines()
 e = stderr.readlines()
