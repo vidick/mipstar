3af7ce5fd89294e089cf552c7cbd977b649c274e
Christopher Zhou <czhou3@caltech.edu>
2020-12-15 21:38:14 -0600

    dont cache pdfs

--- gerby-website/gerby/application.py
+++ gerby-website/gerby/application.py
@@ -164,7 +164,7 @@ def show_chapters():
 
 @app.route('/download/<filename>')
 def send_pdf(filename):
-  return send_from_directory(os.path.join(app.static_folder, 'pdfs'), filename)
+  return send_from_directory(os.path.join(app.static_folder, 'pdfs'), filename, cache_timeout=0)
 
 
 @app.route("/robots.txt")
