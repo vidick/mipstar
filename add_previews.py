9a987e1c7c0d936861bbdb46518bf23c3ecf9691
Christopher Zhou <czhou3@caltech.edu>
2021-03-25 10:17:14 -0500

    test

--- add_previews.py
+++ add_previews.py
@@ -27,7 +27,7 @@ for root, dirs, files in os.walk('./latex/make_doc/'):
                 if ref['data-tag']:
                     content = find_ref_doc(ref['data-tag'], root, files)
                     if content:
-                        ref['data-content'] = content
+                        ref['data-content'] = 'test'
                         ref['class'] = 'page-preview'
                         ref['data-animation'] = 'false'
                         ref['data-toggle'] = 'preview'
